82638e664716a2ea4f11ab2abbbd74e209c80c14
Revanth Venkateswar <revanth.jsr@gmail.com>
2024-10-18 18:01:07 +0530

    registration part complete

---
+++ src/main/java/com/revanth/jobportal/repository/UsersTypeRepository.java
@@ -0,0 +1,8 @@
+package com.revanth.jobportal.repository;
+
+import com.revanth.jobportal.entity.UsersType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+
+}
