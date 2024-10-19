05fc6bb62a7e8196d109faed52237c977b27d00a
Revanth Venkateswar <revanth.jsr@gmail.com>
2024-10-19 20:30:32 +0530

    login/logout and recruiter profile completed

--- src/main/java/com/revanth/jobportal/controller/UsersController.java
+++ src/main/java/com/revanth/jobportal/controller/UsersController.java
@@ -4,8 +4,13 @@ import com.revanth.jobportal.entity.Users;
 import com.revanth.jobportal.entity.UsersType;
 import com.revanth.jobportal.services.UsersService;
 import com.revanth.jobportal.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,6 +53,20 @@ public class UsersController {
         }
 
         usersService.addNew(users);
-        return "dashboard";
+        return "redirect:/dashboard/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 }
